3e81f64569bc44c5ef9f8ae7b80741bad072277b
Brandon_Pampuch <brandonpampuch@gmail.com>
2019-11-10 18:14:54 -0600

    add initial client services

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,31 @@
 // src/App.js
 
 import React from "react";
+
+//components
 import NavBar from "./components/NavBar";
-import { useAuth0 } from "./react-auth0-spa";
+import PrivateRoute from "../src/components/PriaveRoute"
+import ExternalApi from "./components/ExternalApi";
+import Profile from "./components/Profile";
+
+//routing
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
 
 function App() {
-  const { loading } = useAuth0();
-
-  if (loading) {
-    return (
-      <div>Loading...</div>
-    );
-  }
-
   return (
     <div className="App">
-      <header>
-        <NavBar />
-      </header>
+      {/* New - use BrowserRouter to provide access to /profile */}
+      <BrowserRouter>
+        <header>
+          <NavBar />
+        </header>
+        <Switch>
+          <Route path="/" exact />
+          <PrivateRoute path="/profile" component={Profile} />
+          <PrivateRoute path="/external-api" component={ExternalApi} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { useAuth0 } from "../react-auth0-spa";
+import { Link } from "react-router-dom";
+
 
 const NavBar = () => {
     const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
@@ -13,12 +15,19 @@ const NavBar = () => {
                     }
                 >
                     Log in
-        </button>
+                </button>
             )}
 
             {isAuthenticated && <button onClick={() => logout()}>Log out</button>}
+            {isAuthenticated && (
+                <span>
+                    <Link to="/">Home</Link>&nbsp;
+                    <Link to="/profile">Profile</Link>
+                    <Link to="/external-api">External API</Link>
+                </span>
+            )}
         </div>
-    );
-};
+    )
+}
 
 export default NavBar;
